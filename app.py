88bd97f70880d66a89af9dad49484bb36b219e47
dakshdedha0001 <140307293+dakshdedha0001@users.noreply.github.com>
2026-04-23 23:03:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 
 # ---------------- ENV ----------------
 load_dotenv()
-GROQ_API_KEY = os.getenv("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")
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 
 client = Groq(api_key=GROQ_API_KEY)
 MODEL = "llama-3.1-8b-instant"
